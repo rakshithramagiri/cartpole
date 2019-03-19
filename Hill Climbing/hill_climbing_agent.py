cf452dabf7f66546118b5801e367a7cf707a5f99
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-19 09:57:36 +0530

    update : started HC_AGENT class

--- Hill Climbing/hill_climbing_agent.py
+++ Hill Climbing/hill_climbing_agent.py
@@ -1,3 +1,15 @@
 import numpy as np
 
 from policy import POLICY
+
+
+class HC_AGENT:
+    def __init__(self, state_size, action_size, seed):
+        self.state_size = state_size
+        self.action_size = action_size
+        np.random.seed(seed)
+
+        self.policy = POLICY(self.state_size, self.action_size, seed)
+
+
+    
