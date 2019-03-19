78647004fc30f2a3be0c9bca4a2c037a282cc833
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-19 20:41:52 +0530

    solved & reorg : solved cartpole-v1 in 192 episodes and moved to folder

---
+++ Hill Climbing/CartPole-v1/policy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class POLICY:
+    def __init__(self, state_size, action_size, seed):
+        self.state_size = state_size
+        self.action_size = action_size
+        np.random.seed(seed)
+
+        self.weights = 1e-4 * np.random.randn(self.state_size, self.action_size)
+
+
+    def forward(self, state):
+        output = np.dot(state, self.weights)
+        output = np.exp(output) / np.sum(np.exp(output)) # Softmax Activation
+        return output
