a63e671f76fe3d87f3db9bfe81b0a248a69c9a37
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-03-19 09:55:27 +0530

    update : changed policy class name

--- Hill Climbing/policy.py
+++ Hill Climbing/policy.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class Policy:
+class POLICY:
     def __init__(self, state_size, action_size, seed):
         self.state_size = state_size
         self.action_size = action_size
