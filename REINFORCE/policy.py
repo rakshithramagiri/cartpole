d979185bb8df0ded1397800ae861255990997930
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-04-19 22:05:13 +0530

    update : finished policy class

--- REINFORCE/policy.py
+++ REINFORCE/policy.py
@@ -1,2 +1,33 @@
 import torch
+import numpy as np
 import torch.nn as nn
+
+
+class Policy(nn.Module):
+    def __init__(self, state_space, hidden=16, action_space):
+        super(Policy, self).__init__()
+
+        self.state_space = state_space
+        self.hidden = hidden
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.state_space, self.hidden),
+            nn.ReLU(),
+
+            nn.Linear(self.hidden, action_space)
+        )
+
+        self.softmax = nn.Softmax(dim=1)
+
+
+    def forward(self, state):
+        output = self.fc(state)
+        return self.softmax(output)
+
+
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        log_probs = self.forward(state)
+        action_distribution = torch.distributions.Categorical(log_probs)
+        action = action_distribution.sample()
+        return action.item(), action_distribution.log_prob(action)
