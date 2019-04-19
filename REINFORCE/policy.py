36f9239c8f3796211b6c0bddb5c1130431ed1d80
Rakshith Ramagiri <rakshithramagiri96@gmail.com>
2019-04-19 22:22:13 +0530

    update : moved few functions to reinforce agent file

--- REINFORCE/policy.py
+++ REINFORCE/policy.py
@@ -4,11 +4,12 @@ import torch.nn as nn
 
 
 class POLICY(nn.Module):
-    def __init__(self, state_space, action_space, hidden=16):
+    def __init__(self, state_space, action_space, seed, hidden=16):
         super(Policy, self).__init__()
 
         self.state_space = state_space
         self.hidden = hidden
+        torch.manual_seed(seed)
 
         self.fc = nn.Sequential(
             nn.Linear(self.state_space, self.hidden),
@@ -23,11 +24,3 @@ class POLICY(nn.Module):
     def forward(self, state):
         output = self.fc(state)
         return self.softmax(output)
-
-
-    def act(self, state):
-        state = torch.from_numpy(state).float().unsqueeze(0)
-        log_probs = self.forward(state)
-        action_distribution = torch.distributions.Categorical(log_probs)
-        action = action_distribution.sample()
-        return action.item(), action_distribution.log_prob(action)
